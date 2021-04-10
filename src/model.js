edd8cb207892b9adee3f9d1604da47237edacf47
Drainking <heavyyrain3039@gmail.com>
2021-04-11 01:12:41 +0300

    Декомпозиция

---
+++ src/model.js
@@ -0,0 +1,12 @@
+export const model = [
+    { type: 'title', value: 'Hello World from JS' },
+    { type: 'text', value: 'here we go with some text' },
+    {
+        type: 'columns', value: [
+            '111111',
+            '222222',
+            '333333',
+        ]
+    },
+    {type: 'image', value: './assets/image.jpg'}
+]
