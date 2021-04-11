52ca4af37e3584780bf46c2f5410a8164f2143a3
Drainking <heavyyrain3039@gmail.com>
2021-04-11 13:59:51 +0300

    Создание классов

--- src/model.js
+++ src/model.js
@@ -1,7 +1,8 @@
 import image from './assets/image.jpg'
+import {Block} from './class/blocks'
 
 export const model = [
-    { type: 'title', value: 'Конструктор сайтов на JavaScript', options: {
+    new Block('title', 'Конструктор сайтов на JavaScript', {
         tag: 'h2',
         styles: {
             background: 'linear-gradient(to right, #ff0099, #493240)',
@@ -9,14 +10,35 @@ export const model = [
             padding: '1.5rem',
             'text-align': 'center'
         }
-    }},
-    { type: 'text', value: 'here we go with some text' },
-    {
-        type: 'columns', value: [
-            '111111',
-            '222222',
-            '333333',
-        ]
-    },
-    {type: 'image', value: image}
+    }),
+    new Block('image', image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto'            
+        }
+    }),
+    new Block('columns', [
+        'Приложение на чистом JavaScript, без использования библиотек',
+        'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+        'JavaScript - это просто, интересно. Научись создавать любые UI своими руками',
+    ], {
+        styles: {
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+            padding: '2rem',
+            'font-weight': 'bold',
+            color: '#fff'
+        }
+    }),
+    new Block('text', 'here we go with some text', {
+        styles: {
+            background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+            padding: '1rem',
+            'font-weight': 'bold'
+        }
+    })
 ]
